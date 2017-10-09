fb18f5513c457fd975e070b071fcf1ada97c2b9f
John T Cronin <jt.cronin@outlook.com>
2017-10-08 21:44:43 -0500

    setting up the board

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,29 +4,34 @@ import './index.css';
 
 
 class Square extends React.Component {
+	  render() {
+	      return (
+	        <button className="square" onClick={() => this.props.onClick()}>
+	          {this.props.value}
+	        </button>
+	      );
+	    }
+	  }
+
+class Board extends React.Component {
 
 	constructor(props) {
 	    super(props);
 	    this.state = {
-	      value: null,
+	      squares: Array(9).fill(null),
 	    };
 	  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-      {this.state.value}
-      </button>
-    );
-  }
-}
+  	renderSquare(i) {
+  		return (
+    	<Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      	/>
+  		);
+  	}
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i}/>
-  };
-
-  render() {
-    const status = 'Next player: X';
+  	render() {
+    	const status = 'Next player: X';
 
     return (
       <div>
