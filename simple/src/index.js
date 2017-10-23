9ba084875518b7fae2c9470d934c1b8baf702385
John T Cronin <jt.cronin@outlook.com>
2017-10-23 17:57:49 -0500

    ...we are getting there

--- simple/src/index.js
+++ simple/src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Create a component named MessageComponent
+class VacancySign extends React.Component{
+	render(){ 
+	var text;
+    if (this.props.hasvacancy) {
+      text = 'Vacancy';
+    } else {
+      text = 'No Vacancy';
+    }
+
+	return (
+
+		<div>{text}</div>
+
+		);
+	}
+}
+
+/**
+// Here is another implementation.
+var VacancySign = React.createClass({
+  render: function() {
+    // Notice how we use a ternary expression, which is a valid JavaScript expression.
+    // Uncomment and try using this implementation. Inspect the DOM result. How is it different?
+    return <div>{this.props.hasvacancy ? '' : 'No '}Vacancy</div>;
+  }
+});
+**/
+ReactDOM.render(
+  <VacancySign hasvacancy={true}/>,
+  document.getElementById('root')
+);
